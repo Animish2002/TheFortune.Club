716f256fe6d52741609664d629aedb90b91c3fa0
Animish2002 <animishchopade123@gmail.com>
2025-09-04 12:17:55 +0530

    new changes

--- src/LandingPage/Home.jsx
+++ src/LandingPage/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
       {/* <StatisticsSection /> */}
       <ServicesAndProcess />
       <ProcessSection />
-      <MeetTheTeam />
+      {/* <MeetTheTeam /> */}
       <TechStackSection />
       <ProjectsGallery />
       <ContactUs />
